bd0e67168bc83656ab09b7a1254843c87cd3ebb4
pmpasta2008 <muehlhoe@web.de>
2014-04-02 17:02:24 +0200

    test upload

---
+++ WebApplication/Controllers/CMS_ContentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class CMS_ContentController : Controller
+    {
+        public ContentResult UpdateHtml()    // Actionmethod:: Index
+        {
+
+            return new ContentResult()
+            {
+                Content = "",
+                ContentType = "text/html"
+            };
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
